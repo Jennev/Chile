16c2b659a53c0ca9a78000dfd92cbd30ea85e460
Jennifer Espinoza <jennespinozavidal@gmail.com>
2022-01-01 22:11:50 -0300

    tooltip modificados, alert js agregada

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+var correo = $("#enviarCorreo")
+
+$(correo).click (function(){
+    alert ('¡Gracias!. \n Su mensaje ha sido enviado de manera exitosa')    
+})
+
